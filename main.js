985cb20f3e38e01d9e18784b4b4fa836044fcd43
εяяoя <49128357+stef2106@users.noreply.github.com>
2023-01-29 12:02:16 +0400

    wesh

--- main.js
+++ main.js
@@ -85,7 +85,7 @@ btn.onclick = function () {
             letters+=html1[k]+"\n";
             nb=0;
           }
-        //sdpidssdihsdp
+        //sdpidssdihsdp---------------
       }
       textToCopy=letters;
       console.log(letters);
